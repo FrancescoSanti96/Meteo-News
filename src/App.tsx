151958939995d1eee87fc9a5b16ac0079a95c35f
Francesco <francescosanti123@gmail.com>
2022-07-27 11:26:05 +0200

    add Insert the city

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,23 @@ import React from 'react';
 function App() {
   return (
     <>
-    <h1 className='text-send'>Test tailwind css</h1>
-    </>
+    <div className='container mx-auto pt-2 px-5 lg:px-60 '>
+
+    {/* Weather */}
+    {/* Insert the city */}
+    <div className='flex'>
+    <input
+          type="text"
+          className="text-xl sm:text-xxl block pl-2 h-12 rounded-md focus-visible:outline-primaryBg" 
+          placeholder="Inserisci la città"
+          />
+    <button className=" text-primaryBg bg-send hover:bg-sendHover font-bold h-12 py-0 px-2 border-transparent bg-transparent text-m rounded-md">
+    Cerca
+    </button>
+    </div>
+
+</div>
+    </> 
   );
 }
 
