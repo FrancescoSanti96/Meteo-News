46af3b5231bc62e1f5200176670d23c42d8e371b
Francesco <francescosanti123@gmail.com>
2022-07-29 10:44:41 +0200

    add icons filternews  and the  logic

--- src/components/news/index.tsx
+++ src/components/news/index.tsx
@@ -3,6 +3,12 @@ import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { TailSpin } from "react-loader-spinner";
 import { getNews } from "../../redux/reducers/newsSlice";
+import {
+  CashIcon,
+  BeakerIcon,
+  ChipIcon,
+  ArrowNarrowRightIcon,
+} from "@heroicons/react/outline";
 
 function News() {
   const [loader, setLoader] = useState<boolean>(true);
@@ -18,11 +24,13 @@ function News() {
 
   //Function for consume the API and get the data.
   const sendNews = async () => {
-    // if the prezis are already saved in the store I show them otherwise I make the API call.
+    // if the list are already saved in the store I show them otherwise I make the API call,
+    // this case solve the problem if we add another page for not reload the date.
     if (list.length === 0) {
       try {
         const resp = await axios.get(
-          `http://api.mediastack.com/v1/news?access_key=879bfe2758539fd63d1d8436d559ddb4&countries=it&sources=it&limit=5`
+          // "fff"
+          `http://api.mediastack.com/v1/news?access_key=879bfe2758539fd63d1d8436d559ddb4&countries=it&languages=it&limit=5`
         );
         dispatch(getNews(resp?.data?.data));
         console.log(resp?.data?.data);
@@ -36,16 +44,58 @@ function News() {
     }
   };
 
+  //Function for get the news of a specific category but in english beacuse in italian dont't retrive date.
+  const sendNewsCategory = async (category: string) => {
+    try {
+      const resp = await axios.get(
+        `http://api.mediastack.com/v1/news?access_key=879bfe2758539fd63d1d8436d559ddb4&languages=en&categories=${category}&limit=5`
+      );
+      dispatch(getNews(resp?.data?.data));
+      setLoader(false);
+    } catch (error: any) {
+      console.log(error.data);
+      alert(`Error: ${error?.message || ""}`);
+    }
+  };
+
   return (
     <>
       <div className="flex">
-        <h1 className="bg-secondaryBg text-xl sm:text-xxl">News</h1>
-        <h1 className="bg-secondaryBg text-xl sm:text-xxl ml-8">I</h1>
-        <h1 className="bg-secondaryBg text-xl sm:text-xxl ml-6">I</h1>
-        <h1 className="bg-secondaryBg text-xl sm:text-xxl ml-6">I</h1>
+        <h1 className=" text-xxl mr-10">News</h1>
+        {/* Show news of specif categroies */}
+        <button
+          //onClick call the function and pass the specific categorie
+          onClick={() => {
+            sendNewsCategory("science");
+          }}
+        >
+          <BeakerIcon
+            className="h-10 w-10 sm:h-12 sm:w-12 inline mr-3"
+            aria-hidden="true"
+          />
+        </button>
+        <button
+          onClick={() => {
+            sendNewsCategory("business");
+          }}
+        >
+          <CashIcon
+            className="h-10 w-10 sm:h-12 sm:w-12 inline mr-3"
+            aria-hidden="true"
+          />
+        </button>
+        <button
+          onClick={() => {
+            sendNewsCategory("technology");
+          }}
+        >
+          <ChipIcon
+            className="h-10 w-10 sm:h-12 sm:w-12 inline"
+            aria-hidden="true"
+          />
+        </button>
       </div>
 
-   
       {/* Loading weather data from API */}
       {loader && (
         <span>
@@ -58,10 +108,10 @@ function News() {
         </span>
       )}
 
-      <div className="flex justify-center mt-5">
+      <div className="flex justify-center mt-5 flex-col sm:flex-row items-center">
+        {/* Use Map function for retrive from the list every item 'news' and visualize the card with the information */}
         {list?.map((it: any, index: any) => {
           return (
-            // <div className="">
             <div className="border-solid border-2 border-secondary w-56 h-96 relative">
               <img
                 className="rounded-t-lg px-5 py-2"
@@ -72,7 +122,7 @@ function News() {
                 <h6 className="mb-2  font-bold tracking-tight text-primary ">
                   {it.title}
                 </h6>
-                <div className="text-xs font-bold uppercase text-teal-700 mt-1 mb-2">
+                <div className="text-xxs uppercase text-teal-700 mt-1 mb-2">
                   {it.author}
                 </div>
                 <a
@@ -80,18 +130,7 @@ function News() {
                   className="absolute bottom-0 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   Read more
-                  <svg
-                    className="ml-2 -mr-1 w-4 h-4"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
+                  <ArrowNarrowRightIcon className="h-5 w-6" />
                 </a>
               </div>
             </div>
